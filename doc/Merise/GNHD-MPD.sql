f7ccbe7a1c304ef20933d58200e520f0e5eeb2e7
Alix Allegre <96184685+alixsanta@users.noreply.github.com>
2024-08-31 11:29:22 +0200

    Update GNHD-MPD.sql
    Update int type for postgres & uuid type

--- doc/Merise/GNHD-MPD.sql
+++ doc/Merise/GNHD-MPD.sql
@@ -1,29 +1,41 @@
-CREATE TABLE Users(
-   UUID_User VARCHAR(50),
+CREATE TABLE Users (
+   UUID_User UUID PRIMARY KEY,
    first_name_user VARCHAR(50) NOT NULL,
    last_name_user VARCHAR(50) NOT NULL,
    image_profil VARCHAR(100),
    mail_user VARCHAR(255) NOT NULL,
    password_user VARCHAR(250) NOT NULL,
-   PRIMARY KEY(UUID_User)
+   roles_user TEXT NOT NULL
 );
 
-CREATE TABLE Message(
-   UUID_message INT,
-   message TEXT NOT NULL,
-   PRIMARY KEY(UUID_message)
-);
-
-CREATE TABLE Administrator(
-   UUID_Administrator VARCHAR(50),
-   UUID_User VARCHAR(50) NOT NULL,
-   PRIMARY KEY(UUID_Administrator),
-   UNIQUE(UUID_User),
+CREATE TABLE Publication(
+   UUID_publication UUID PRIMARY KEY,
+   creation_date DATETIME NOT NULL,
+   title_publication VARCHAR(100) NOT NULL,
+   description_publication TEXT,
+   UUID_User UUID NOT NULL,
+   PRIMARY KEY(id_publication),
    FOREIGN KEY(UUID_User) REFERENCES Users(UUID_User)
 );
 
+CREATE TABLE ResidentGroup(
+   UUID_group UUID PRIMARY KEY,
+   name_group VARCHAR(50) NOT NULL,
+   image_group VARCHAR(255),
+   id_address SERIAL NOT NULL,
+   UUID_User UUID NOT NULL,
+   PRIMARY KEY(UUID_group),
+   FOREIGN KEY(id_address) REFERENCES Address(id_address),
+   FOREIGN KEY(UUID_User) REFERENCES Users(UUID_User)
+);
+
+CREATE TABLE Message (
+   UUID_message SERIAL PRIMARY KEY,
+   message TEXT NOT NULL
+);
+
 CREATE TABLE Address(
-   id_address INT,
+   id_address SERIAL PRIMARY KEY,
    number_street VARCHAR(50) NOT NULL,
    name_street VARCHAR(50) NOT NULL,
    postal_code VARCHAR(15) NOT NULL,
@@ -31,43 +43,22 @@ CREATE TABLE Address(
    PRIMARY KEY(id_address)
 );
 
-CREATE TABLE ResidentGroup(
-   UUID_group VARCHAR(50),
-   name_group VARCHAR(50) NOT NULL,
-   image_group VARCHAR(255),
-   id_address INT NOT NULL,
-   UUID_User VARCHAR(50) NOT NULL,
-   PRIMARY KEY(UUID_group),
-   FOREIGN KEY(id_address) REFERENCES Address(id_address),
-   FOREIGN KEY(UUID_User) REFERENCES Users(UUID_User)
-);
-
 CREATE TABLE Comment(
-   id_comment INT,
+   id_comment SERIAL,
    comment TEXT NOT NULL,
    UUID_User VARCHAR(50) NOT NULL,
    PRIMARY KEY(id_comment),
    FOREIGN KEY(UUID_User) REFERENCES Users(UUID_User)
 );
 
-CREATE TABLE Publication(
-   id_publication INT,
-   creation_date DATETIME NOT NULL,
-   title_publication VARCHAR(100) NOT NULL,
-   description_publication TEXT,
-   UUID_User VARCHAR(50) NOT NULL,
-   PRIMARY KEY(id_publication),
-   FOREIGN KEY(UUID_User) REFERENCES Users(UUID_User)
-);
-
 CREATE TABLE Category(
-   id_category INT,
+   id_category SERIAL,
    category VARCHAR(50) NOT NULL,
    PRIMARY KEY(id_category)
 );
 
 CREATE TABLE DateEvent(
-   id_date_event INT,
+   id_date_event SERIAL,
    start_date DATETIME NOT NULL,
    end_date DATETIME,
    id_publication INT NOT NULL,
@@ -77,7 +68,7 @@ CREATE TABLE DateEvent(
 );
 
 CREATE TABLE Price(
-   id_price INT,
+   id_price SERIAL,
    price DECIMAL(5,2) NOT NULL,
    id_publication INT NOT NULL,
    PRIMARY KEY(id_price),
@@ -86,7 +77,7 @@ CREATE TABLE Price(
 );
 
 CREATE TABLE SubCategory(
-   id_sub_category INT,
+   id_sub_category SERIAL,
    sub_category VARCHAR(50) NOT NULL,
    id_category INT NOT NULL,
    PRIMARY KEY(id_sub_category),
@@ -94,10 +85,10 @@ CREATE TABLE SubCategory(
 );
 
 CREATE TABLE Illustration(
-   id_illustration INT,
+   id_illustration SERIAL,
    illustration VARCHAR(255) NOT NULL,
-   id_publication INT,
-   id_comment INT,
+   id_publication SERIAL,
+   id_comment SERIAL,
    PRIMARY KEY(id_illustration),
    UNIQUE(id_comment),
    FOREIGN KEY(id_publication) REFERENCES Publication(id_publication),
@@ -105,16 +96,16 @@ CREATE TABLE Illustration(
 );
 
 CREATE TABLE Recieve(
-   UUID_User VARCHAR(50),
-   UUID_message INT,
+   UUID_User UUID,
+   UUID_message UUID,
    PRIMARY KEY(UUID_User, UUID_message),
    FOREIGN KEY(UUID_User) REFERENCES Users(UUID_User),
    FOREIGN KEY(UUID_message) REFERENCES Message(UUID_message)
 );
 
 CREATE TABLE Send(
-   UUID_User VARCHAR(50),
-   UUID_message INT,
+   UUID_User UUID,
+   UUID_message UUID,
    PRIMARY KEY(UUID_User, UUID_message),
    FOREIGN KEY(UUID_User) REFERENCES Users(UUID_User),
    FOREIGN KEY(UUID_message) REFERENCES Message(UUID_message)
@@ -129,16 +120,16 @@ CREATE TABLE Belong(
 );
 
 CREATE TABLE Link(
-   id_publication INT,
-   id_category INT,
+   id_publication SERIAL,
+   id_category SERIAL,
    PRIMARY KEY(id_publication, id_category),
    FOREIGN KEY(id_publication) REFERENCES Publication(id_publication),
    FOREIGN KEY(id_category) REFERENCES Category(id_category)
 );
 
 CREATE TABLE post_comment_publication(
-   id_comment INT,
-   id_publication INT,
+   id_comment SERIAL,
+   id_publication SERIAL,
    PRIMARY KEY(id_comment, id_publication),
    FOREIGN KEY(id_comment) REFERENCES Comment(id_comment),
    FOREIGN KEY(id_publication) REFERENCES Publication(id_publication)
